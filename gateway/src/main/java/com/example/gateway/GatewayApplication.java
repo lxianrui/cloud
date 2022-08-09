6c4256b291b2060da792cb7ca82c22fa559bea9f
Jeffery Liu <jeff16@foxmail.com>
2022-08-10 00:40:05 +0800

    init

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.example.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关应用程序
+ *
+ * @author Jeffrey Liu
+ * @date 2022/08/09
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
